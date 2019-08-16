289f50f7a1ab3c7635eafe221a37fd53482fbf0f
TheNsBhasin <nsbhasincool@gmail.com>
2019-08-16 20:26:22 +0530

    Fix Cursor scaling on anchor tags

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,27 +1,23 @@
-import React, { Component, Fragment } from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-library.add(fab);
+import React, {Component, Fragment} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faYoutube, faFacebookSquare, faInstagram} from '@fortawesome/free-brands-svg-icons'
+
 class Footer extends Component {
     render() {
         return (
             <Fragment>
                 <div className={"flex-center social-networks bounce"}>
-                    <a href={"https://www.youtube.com/watch?v=BWMzcA-nBQU"}
-                        target={"_blank"}
-                        className={"my-youtube"}>
-                        <FontAwesomeIcon icon={['fab', 'youtube']} size={"3x"} />
+                    <a href={"https://www.youtube.com/watch?v=BWMzcA-nBQU"} target={"_blank"}>
+                        <FontAwesomeIcon icon={faYoutube}
+                                         size="3x"/>
                     </a>
-                    <a href={"https://www.facebook.com/pecfestofficial/"}
-                        target={"_blank"}
-                        className={"my-facebook"}>
-                        <FontAwesomeIcon icon={['fab', 'facebook-square']} size={"3x"} />
+                    <a href={"https://www.facebook.com/pecfestofficial/"} target={"_blank"}>
+                        <FontAwesomeIcon icon={faFacebookSquare}
+                                         size="3x"/>
                     </a>
-                    <a href={"https://www.instagram.com/pec.pecfest/"}
-                        target={"_blank"}
-                        className={"my-instagram"}>
-                        <FontAwesomeIcon icon={['fab', 'instagram']} size={"3x"} />
+                    <a href={"https://www.instagram.com/pec.pecfest/"} target={"_blank"}>
+                        <FontAwesomeIcon icon={faInstagram}
+                                         size="3x"/>
                     </a>
                 </div>
             </Fragment>
