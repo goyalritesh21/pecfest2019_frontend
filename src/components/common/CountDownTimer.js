f2bcffcd31c172c4b47c0dbc2a443413fc03534c
TheNsBhasin <nsbhasincool@gmail.com>
2019-08-16 19:00:26 +0530

    Fix Auth page styling

--- src/components/common/CountDownTimer.js
+++ src/components/common/CountDownTimer.js
@@ -1,6 +1,57 @@
 import React from 'react';
 import moment from 'moment';
 
+const SVGCircle = ({radius, stroke}) => (
+    <svg className="countdown-svg">
+        <path
+            fill="none"
+            stroke={stroke}
+            strokeWidth="4"
+            d={describeArc(50, 50, 48, 0, radius)}
+        />
+    </svg>
+);
+
+
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+
+    return {
+        x: centerX + radius * Math.cos(angleInRadians),
+        y: centerY + radius * Math.sin(angleInRadians)
+    };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle) {
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+
+    var d = [
+        'M',
+        start.x,
+        start.y,
+        'A',
+        radius,
+        radius,
+        0,
+        largeArcFlag,
+        0,
+        end.x,
+        end.y
+    ].join(' ');
+
+    return d;
+}
+
+
+function mapNumber(number, in_min, in_max, out_min, out_max) {
+    return (
+        ((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    );
+}
+
 class Countdown extends React.Component {
     state = {
         days: undefined,
@@ -89,53 +140,3 @@ class Countdown extends React.Component {
 }
 
 export default Countdown;
-const SVGCircle = ({radius, stroke}) => (
-    <svg className="countdown-svg">
-        <path
-            fill="none"
-            stroke={stroke}
-            strokeWidth="4"
-            d={describeArc(50, 50, 48, 0, radius)}
-        />
-    </svg>
-);
-
-
-function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-    var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
-
-    return {
-        x: centerX + radius * Math.cos(angleInRadians),
-        y: centerY + radius * Math.sin(angleInRadians)
-    };
-}
-
-function describeArc(x, y, radius, startAngle, endAngle) {
-    var start = polarToCartesian(x, y, radius, endAngle);
-    var end = polarToCartesian(x, y, radius, startAngle);
-
-    var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
-
-    var d = [
-        'M',
-        start.x,
-        start.y,
-        'A',
-        radius,
-        radius,
-        0,
-        largeArcFlag,
-        0,
-        end.x,
-        end.y
-    ].join(' ');
-
-    return d;
-}
-
-
-function mapNumber(number, in_min, in_max, out_min, out_max) {
-    return (
-        ((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-    );
-}
