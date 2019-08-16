123081ef100642ad3604515b37ea7d157d7c8516
Ritesh Goyal <goyalritesh21@gmail.com>
2019-08-16 20:14:37 +0530

    Refractor font awesome icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,16 +1,28 @@
-import React, {Component, Fragment} from 'react';
-
+import React, { Component, Fragment } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(fab);
 class Footer extends Component {
     render() {
         return (
             <Fragment>
                 <div className={"flex-center social-networks bounce"}>
-                    <a href={"https://www.youtube.com/watch?v=BWMzcA-nBQU"} target={"_blank"}>
-                        <i className={"fab fa-youtube fa-3x my-youtube"}/></a>
-                    <a href={"https://www.facebook.com/pecfestofficial/"} target={"_blank"}>
-                        <i className="fab fa-facebook-square fa-3x my-facebook"/></a>
-                    <a href={"https://www.instagram.com/pec.pecfest/"} target={"_blank"}>
-                        <i className="fab fa-instagram fa-3x my-instagram"/></a>
+                    <a href={"https://www.youtube.com/watch?v=BWMzcA-nBQU"}
+                        target={"_blank"}
+                        className={"my-youtube"}>
+                        <FontAwesomeIcon icon={['fab', 'youtube']} size={"3x"} />
+                    </a>
+                    <a href={"https://www.facebook.com/pecfestofficial/"}
+                        target={"_blank"}
+                        className={"my-facebook"}>
+                        <FontAwesomeIcon icon={['fab', 'facebook-square']} size={"3x"} />
+                    </a>
+                    <a href={"https://www.instagram.com/pec.pecfest/"}
+                        target={"_blank"}
+                        className={"my-instagram"}>
+                        <FontAwesomeIcon icon={['fab', 'instagram']} size={"3x"} />
+                    </a>
                 </div>
             </Fragment>
         );
