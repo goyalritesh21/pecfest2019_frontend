a2154892cfca64c8171cb3e37d8408be4722947f
Ritesh Goyal <goyalritesh21@gmail.com>
2019-08-28 00:53:23 +0530

    Refractor CountDownTimer

--- src/components/common/CountDownTimer.js
+++ src/components/common/CountDownTimer.js
@@ -1,156 +1,187 @@
-import React from 'react';
-import moment from 'moment';
-import {withGetScreen} from 'react-getscreen'
+import React from "react";
+import moment from "moment";
+import * as PropTypes from "prop-types";
+import { withGetScreen } from "react-getscreen";
 
-const SVGCircle = ({radius, stroke, view}) => {
-    if(view == '1'){
-    return <svg className="countdown-svg">
+const SVGCircle = ({ radius, stroke, view }) => {
+  if (view == "1") {
+    return (
+      <svg className="countdown-svg">
         <path
-            fill="none"
-            stroke={stroke}
-            strokeWidth="4"
-            d={describeArc(50, 50, 48, 0, radius)}
+          fill="none"
+          stroke={stroke}
+          strokeWidth="4"
+          d={describeArc(50, 50, 48, 0, radius)}
         />
-    </svg>
-    }
-    else {
-        return <svg className="countdown-svg">
+      </svg>
+    );
+  } else {
+    return (
+      <svg className="countdown-svg">
         <path
-            fill="none"
-            stroke={stroke}
-            strokeWidth="2"
-            d={describeArc(32, 32, 30, 0, radius)}
+          fill="none"
+          stroke={stroke}
+          strokeWidth="2"
+          d={describeArc(32, 32, 30, 0, radius)}
         />
-    </svg>
-    }
+      </svg>
+    );
+  }
 };
 
-
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-    var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+  var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
 
-    return {
-        x: centerX + radius * Math.cos(angleInRadians),
-        y: centerY + radius * Math.sin(angleInRadians)
-    };
+  return {
+    x: centerX + radius * Math.cos(angleInRadians),
+    y: centerY + radius * Math.sin(angleInRadians)
+  };
 }
 
 function describeArc(x, y, radius, startAngle, endAngle) {
-    var start = polarToCartesian(x, y, radius, endAngle);
-    var end = polarToCartesian(x, y, radius, startAngle);
+  var start = polarToCartesian(x, y, radius, endAngle);
+  var end = polarToCartesian(x, y, radius, startAngle);
 
-    var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+  var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
 
-    var d = [
-        'M',
-        start.x,
-        start.y,
-        'A',
-        radius,
-        radius,
-        0,
-        largeArcFlag,
-        0,
-        end.x,
-        end.y
-    ].join(' ');
+  var d = [
+    "M",
+    start.x,
+    start.y,
+    "A",
+    radius,
+    radius,
+    0,
+    largeArcFlag,
+    0,
+    end.x,
+    end.y
+  ].join(" ");
 
-    return d;
+  return d;
 }
 
-
 function mapNumber(number, in_min, in_max, out_min, out_max) {
-    return (
-        ((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-    );
+  return (
+    ((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  );
 }
 
 class Countdown extends React.Component {
-    state = {
-        days: undefined,
-        hours: undefined,
-        minutes: undefined,
-        seconds: undefined
+  constructor(props) {
+    super(props);
+    const dateTime = this._getDateTime();
+    this.state = {
+      days: dateTime.days,
+      hours: dateTime.hours,
+      minutes: dateTime.minutes,
+      seconds: dateTime.seconds
     };
+  }
+  static propTypes = {
+    timeTillDate: PropTypes.object.isRequired
+  };
 
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            const {timeTillDate} = this.props;
-            const then = moment(timeTillDate).add(98, 'days');
-            const now = moment();
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const dateTime = this._getDateTime();
+      this.setState(() => ({
+        days: dateTime.days,
+        hours: dateTime.hours,
+        minutes: dateTime.minutes,
+        seconds: dateTime.seconds
+      }));
+    }, 1000);
+  }
 
-            const countdown = moment.duration(then.diff(now));
-            const days = Math.floor(countdown.asDays());
-            countdown.subtract(moment.duration(days, 'days'));
-            const hours = countdown.hours();
-            countdown.subtract(moment.duration(hours, 'hours'));
-            const minutes = countdown.minutes();
-            countdown.subtract(moment.duration(minutes, 'minutes'));
-            const seconds = countdown.seconds();
+  _getDateTime() {
+    const { timeTillDate } = this.props;
+    const then = moment(timeTillDate).add(98, "days");
+    const now = moment();
 
-            this.setState({days, hours, minutes, seconds});
-        }, 1000);
+    const countdown = moment.duration(then.diff(now));
+    const days = Math.floor(countdown.asDays());
+    countdown.subtract(moment.duration(days, "days"));
+    const hours = countdown.hours();
+    countdown.subtract(moment.duration(hours, "hours"));
+    const minutes = countdown.minutes();
+    countdown.subtract(moment.duration(minutes, "minutes"));
+    const seconds = countdown.seconds();
+    const dateTime = {
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    };
+    return dateTime;
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  render() {
+    var view = 1;
+    if (this.props.isMobile()) view = 0;
+    const { days, hours, minutes, seconds } = this.state;
+
+    // Mapping the date values to radius values
+    const daysRadius = mapNumber(days, 100, 0, 0, 360);
+    const hoursRadius = mapNumber(hours, 24, 0, 0, 360);
+    const minutesRadius = mapNumber(minutes, 60, 0, 0, 360);
+    const secondsRadius = mapNumber(seconds, 60, 0, 0, 360);
+
+    if (days < 0) {
+      return (
+        <div>
+          <h2>Currently going on!</h2>
+        </div>
+      );
     }
 
-    componentWillUnmount() {
-        if (this.interval) {
-            clearInterval(this.interval);
-        }
-    }
-
-    render() {
-        var view = 1;
-        if (this.props.isMobile())
-           view = 0;
-        const {days, hours, minutes, seconds} = this.state;
-        
-        // Mapping the date values to radius values
-        const daysRadius = mapNumber(days, 100, 0, 0, 360);
-        const hoursRadius = mapNumber(hours, 24, 0, 0, 360);
-        const minutesRadius = mapNumber(minutes, 60, 0, 0, 360);
-        const secondsRadius = mapNumber(seconds, 60, 0, 0, 360);
-
-        if (days < 0) {
-            return (
-                <div>
-                    <h2>Currently going on!</h2>
-                </div>
-            );
-        }
-
-        return (
-            <div className="countdown-wrapper">
-                {days >= 0 && (
-                    <div className="countdown-item">
-                        <SVGCircle radius={daysRadius} stroke="#32CD32"  view={view}/>
-                        {days}
-                        <span className={"subTextCount"} style={{color: "#ffffff"}}>days</span>
-                    </div>
-                )}
-                {hours >= 0 && (
-                    <div className="countdown-item">
-                        <SVGCircle radius={hoursRadius} stroke="#87CEEB" view={view}/>
-                        {hours}
-                        <span className={"subTextCount"} style={{color: "#ffffff"}}>hours</span>
-                    </div>
-                )}
-                {minutes >= 0 && (
-                    <div className="countdown-item">
-                        <SVGCircle radius={minutesRadius} stroke="#4B0082" view={view}/>
-                        {minutes}
-                        <span className={"subTextCount"} style={{color: "#ffffff"}}>minutes</span>
-                    </div>
-                )}
-                {seconds >= 0 && (
-                    <div className="countdown-item">
-                        <SVGCircle radius={secondsRadius} stroke="#FFA500" view={view}/>
-                        {seconds}
-                        <span className={"subTextCount"} style={{color: "#ffffff"}}>seconds</span>
-                    </div>
-                )}
-            </div>
-        );
-    }
+    return (
+      <div className="countdown-wrapper">
+        {days >= 0 && (
+          <div className="countdown-item">
+            <SVGCircle radius={daysRadius} stroke="#32CD32" view={view} />
+            {days}
+            <span className={"subTextCount"} style={{ color: "#ffffff" }}>
+              days
+            </span>
+          </div>
+        )}
+        {hours >= 0 && (
+          <div className="countdown-item">
+            <SVGCircle radius={hoursRadius} stroke="#87CEEB" view={view} />
+            {hours}
+            <span className={"subTextCount"} style={{ color: "#ffffff" }}>
+              hours
+            </span>
+          </div>
+        )}
+        {minutes >= 0 && (
+          <div className="countdown-item">
+            <SVGCircle radius={minutesRadius} stroke="#4B0082" view={view} />
+            {minutes}
+            <span className={"subTextCount"} style={{ color: "#ffffff" }}>
+              minutes
+            </span>
+          </div>
+        )}
+        {seconds >= 0 && (
+          <div className="countdown-item">
+            <SVGCircle radius={secondsRadius} stroke="#FFA500" view={view} />
+            {seconds}
+            <span className={"subTextCount"} style={{ color: "#ffffff" }}>
+              seconds
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default withGetScreen(Countdown);
