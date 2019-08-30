044c41f116e226d883d5cdbd77af53c7c219fb7e
Ritesh Goyal <goyalritesh21@gmail.com>
2019-08-30 12:59:07 +0530

    Password validation done

--- src/components/accounts/Login.js
+++ src/components/accounts/Login.js
@@ -1,133 +1,154 @@
-import React, {Component} from 'react'
-import {Link, Redirect, withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-import {login} from '../../actions/auth';
+import React, { Component } from "react";
+import { Link, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { createMessage } from "../../actions/messages";
+import { login } from "../../actions/auth";
 import Loader from "../common/Loader";
-import anime from 'animejs';
+import anime from "animejs";
+
+const passwordPattern = "(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}";
 
 export class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
-            username: '',
-            password: '',
-        };
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  static propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+    user: PropTypes.object,
+    isLoading: PropTypes.bool.isRequired,
+    createMessage: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    const timeline = anime.timeline();
+    timeline.add({
+      targets: ".main, .form-group, #register",
+      translateY: [100, 0],
+      opacity: [0, 1],
+      duration: 1000,
+      easing: "easeOutElastic",
+      delay: (el, i, l) => i * 200
+    });
+  }
+
+  onChange = e => {
+    const key = e.target.name;
+    const val = e.target.value;
+    if (key === "username2019") {
+      this.setState(() => ({ username: val.toUpperCase() }));
+    } else {
+      this.setState(() => ({ password: val }));
     }
+  };
 
-    static propTypes = {
-        login: PropTypes.func.isRequired,
-        isAuthenticated: PropTypes.bool,
-        user: PropTypes.object,
-        isLoading: PropTypes.bool.isRequired
-    };
-
-    componentDidMount() {
-        const timeline = anime.timeline();
-        timeline.add({
-            targets: '.main, .form-group, #register',
-            translateY: [100, 0],
-            opacity: [0, 1],
-            duration: 1000,
-            easing: 'easeOutElastic',
-            delay: (el, i, l) => i * 200
-        });
+  onSubmit = e => {
+    e.preventDefault();
+    const { username, password } = this.state;
+    let errors = [];
+    if (!password.match(passwordPattern)) {
+      errors.push(
+        "Password must contain at least one number, one uppercase, lowercase letter, and at least 8 or more characters"
+      );
     }
+    if (errors.length > 0) {
+      const updateErrorMessage = errors.join("\n");
+      this.props.createMessage({ updateErrorMessage });
+      return;
+    }
+    this.props.login(username.toLowerCase(), password);
+  };
 
-    onChange = e => {
-        const key = e.target.name;
-        const val = e.target.value;
-        if (key === 'username2019') {
-            this.setState(() => ({username: val.toUpperCase()}));
-        } else {
-            this.setState(() => ({password: val}));
-        }
+  render() {
+    const { isAuthenticated, user, isLoading } = this.props;
+    if (isAuthenticated) {
+      if (user !== null && !user.participant.firstTimer) {
+        return <Redirect to="/events" />;
+      } else {
+        return <Redirect to="/update" />;
+      }
+    }
+    const { username, password } = this.state;
+    const randUserId = "username2019";
+    const randPassId = "password2019";
 
-    };
-
-    onSubmit = e => {
-        e.preventDefault();
-        const {username, password} = this.state;
-        this.props.login(username.toLowerCase(), password);
-    };
-
-    render() {
-        const {isAuthenticated, user, isLoading} = this.props;
-        if (isAuthenticated) {
-            if (user !== null && !user.participant.firstTimer) {
-
-                return <Redirect to="/events"/>
-            } else {
-                return <Redirect to="/update"/>
-            }
-        }
-        const {username, password} = this.state;
-        const randUserId = "username2019";
-        const randPassId = "password2019";
-
-        return (
-            <div>
-                {isLoading ? <Loader/> : (
-                    <div className="col-md-6 m-auto upper-padding">
-                        <div className="mt-5 main">
-                            <h2 className="text-center">Login</h2>
-                            <br/>
-                            <form onSubmit={this.onSubmit}>
-                                <div className="form-group">
-                                    <label htmlFor="username">Username</label>
-                                    <div className="input-outer">
-                                        <input
-                                            type="text"
-                                            className="form-control input"
-                                            name={randUserId}
-                                            onChange={this.onChange}
-                                            value={username}
-                                            tabIndex="1"
-                                            id={randUserId}
-                                            spellCheck="false"
-                                            autoComplete="new-username"
-                                            required
-                                        />
-                                    </div>
-                                </div>
-                                <div className="form-group">
-                                    <label>Password</label>
-                                    <div className="input-outer">
-                                        <input
-                                            type="password"
-                                            className="form-control input"
-                                            name={randPassId}
-                                            onChange={this.onChange}
-                                            value={password}
-                                            tabIndex="2"
-                                            id={randPassId}
-                                            autoComplete="new-password"
-                                            spellCheck="false"
-                                            required
-                                        />
-                                    </div>
-                                </div>
-                                <br/>
-                                <div className="form-group">
-                                    <button type="submit" className="btn btn-slide" tabIndex="3">Login
-                                    </button>
-                                </div>
-                                <p id={"register"}>
-                                    Don't have an account? <Link to="/register">Register</Link>
-                                </p>
-                            </form>
-                        </div>
-                    </div>)
-                }
+    return (
+      <div>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="col-md-6 m-auto upper-padding">
+            <div className="mt-5 main">
+              <h2 className="text-center">Login</h2>
+              <br />
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <div className="input-outer">
+                    <input
+                      type="text"
+                      className="form-control input"
+                      name={randUserId}
+                      onChange={this.onChange}
+                      value={username}
+                      tabIndex="1"
+                      id={randUserId}
+                      spellCheck="false"
+                      autoComplete="new-username"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <div className="input-outer">
+                    <input
+                      type="password"
+                      className="form-control input"
+                      name={randPassId}
+                      onChange={this.onChange}
+                      value={password}
+                      tabIndex="2"
+                      id={randPassId}
+                      autoComplete="new-password"
+                      spellCheck="false"
+                      required
+                    />
+                  </div>
+                </div>
+                <br />
+                <div className="form-group">
+                  <button type="submit" className="btn btn-slide" tabIndex="3">
+                    Login
+                  </button>
+                </div>
+                <p id={"register"}>
+                  Don't have an account? <Link to="/register">Register</Link>
+                </p>
+              </form>
             </div>
-        )
-    }
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated,
-    user: state.auth.user,
-    isLoading: state.auth.isLoading
+  isAuthenticated: state.auth.isAuthenticated,
+  user: state.auth.user,
+  isLoading: state.auth.isLoading
 });
 
-export default withRouter(connect(mapStateToProps, {login})(Login));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { login, createMessage }
+  )(Login)
+);
--- src/components/accounts/Register.js
+++ src/components/accounts/Register.js
@@ -1,189 +1,193 @@
-import React, { Component } from 'react'
-import { withRouter, Link, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { register } from '../../actions/auth';
-import { createMessage } from '../../actions/messages';
+import React, { Component } from "react";
+import { withRouter, Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { register } from "../../actions/auth";
+import { createMessage } from "../../actions/messages";
 import Loader from "../common/Loader";
-import anime from 'animejs';
+import anime from "animejs";
+
+const passwordPattern = "(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}";
+
 export class Register extends Component {
-    state = {
-        username: '',
-        email: '',
-        password: '',
-        password2: ''
-    };
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    password2: ""
+  };
 
-    static propTypes = {
-        register: PropTypes.func.isRequired,
-        isAuthenticated: PropTypes.bool,
-        createMessage: PropTypes.func.isRequired,
-        user: PropTypes.object,
-        isLoading: PropTypes.bool.isRequired
-    };
+  static propTypes = {
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+    createMessage: PropTypes.func.isRequired,
+    user: PropTypes.object,
+    isLoading: PropTypes.bool.isRequired
+  };
 
-    componentDidMount() {
-        const timeline = anime.timeline();
-        timeline.add({
-            targets: '.main, .form-group, #login',
-            translateY: [100, 0],
-            opacity: [0, 1],
-            duration: 1000,
-            easing: 'easeOutElastic',
-            delay: (el, i, l) => i * 200
-        });
+  componentDidMount() {
+    const timeline = anime.timeline();
+    timeline.add({
+      targets: ".main, .form-group, #login",
+      translateY: [100, 0],
+      opacity: [0, 1],
+      duration: 1000,
+      easing: "easeOutElastic",
+      delay: (el, i, l) => i * 200
+    });
+  }
+
+  onChange = e => {
+    const key = e.target.name;
+    const val = e.target.value;
+    if (key === "username2019") {
+      this.setState(() => ({ username: val.toUpperCase() }));
+    } else if (key === "email2019") {
+      this.setState(() => ({ email: val }));
+    } else if (key === "password2019") {
+      this.setState(() => ({ password: val }));
+    } else if (key === "password22019") {
+      this.setState(() => ({ password2: val }));
     }
+  };
 
-    onChange = e => {
-        const key = e.target.name;
-        const val = e.target.value;
-        if (key === 'username2019') {
-            this.setState(() => ({ username: val.toUpperCase() }));
-        }
-        else if (key === 'email2019') {
-            this.setState(() => ({ email: val }));
-        }
-        else if (key === 'password2019') {
-            this.setState(() => ({ password: val }));
-        }
-        else if (key === 'password22019') {
-            this.setState(() => ({ password2: val }));
-        }
-    };
+  onSubmit = e => {
+    e.preventDefault();
+    const { username, email, password, password2 } = this.state;
+    let errors = [];
+    if (username.length < 2) {
+      errors.push("Invalid username");
+    }
+    if (!password.match(passwordPattern)) {
+      errors.push(
+        "Password must contain at least one number, one uppercase, lowercase letter, and at least 8 or more characters"
+      );
+    }
+    if (password !== password2) {
+      errors.push("Passwords do not match");
+    }
+    if (errors.length > 0) {
+      const updateErrorMessage = errors.join("\n");
+      this.props.createMessage({ updateErrorMessage });
+      return;
+    }
+    const user = { username: username.toLowerCase(), email, password };
+    this.props.register(user);
+  };
 
-    onSubmit = (e) => {
-        e.preventDefault();
-        const { username, email, password, password2 } = this.state;
-        let errors = [];
-        if (username.length < 2) {
-            errors.push("Invalid username");
-        }
-        if (password.length < 8 || password2.length < 8) {
-            errors.push("Password should be atleast 8 characters long!");
-        }
-        if (password !== password2) {
-            errors.push("Passwords do not match");
-        }
-        if (errors.length > 0) {
-            const updateErrorMessage = errors.join("\n");
-            this.props.createMessage({ updateErrorMessage });
-            return;
-        }
-        const user = { username: username.toLowerCase(), email, password };
-        this.props.register(user);
+  render() {
+    const { isAuthenticated, user, isLoading } = this.props;
+    if (isAuthenticated) {
+      if (user !== null && !user.participant.firstTimer) {
+        return <Redirect to="/" />;
+      } else {
+        return <Redirect to="/update" />;
+      }
+    }
+    if (isLoading) {
+      return <Loader />;
+    }
+    const { username, email, password, password2 } = this.state;
+    const randUserId = "username2019";
+    const randPassId = "password2019";
+    const randEmailId = "email2019";
+    const randPassId2 = "password22019";
 
-
-    };
-
-    render() {
-        const { isAuthenticated, user, isLoading } = this.props;
-        if (isAuthenticated) {
-            if (user !== null && !user.participant.firstTimer) {
-                return <Redirect to="/" />
-            }
-            else {
-                return <Redirect to="/update" />
-            }
-        }
-        if (isLoading) {
-            return (<Loader />)
-        }
-        const { username, email, password, password2 } = this.state;
-        const randUserId = "username2019";
-        const randPassId = "password2019";
-        const randEmailId = "email2019";
-        const randPassId2 = "password22019";
-
-        return (
-            <div className="col-md-6 m-auto upper-padding-register">
-                <div className="main">
-                    <h2 className="text-center">Register</h2>
-                    <br />
-                    <form autoComplete="off" onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                            <label>Username</label>
-                            <div className="input-outer">
-                                <input
-                                    type="text"
-                                    className="form-control input"
-                                    name={randUserId}
-                                    onChange={this.onChange}
-                                    value={username}
-                                    id={randUserId}
-                                    tabIndex="1"
-                                    spellCheck="false"
-                                    autoComplete="new-username"
-                                    required
-                                />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label>Email</label>
-                            <div className="input-outer">
-                                <input
-                                    type="email"
-                                    className="form-control input"
-                                    name={randEmailId}
-                                    onChange={this.onChange}
-                                    value={email}
-                                    tabIndex="2"
-                                    spellCheck="false"
-                                    autoComplete="new-email"
-                                    required
-                                />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label>Password</label>
-                            <div className="input-outer">
-                                <input
-                                    type="password"
-                                    className="form-control input"
-                                    name={randPassId}
-                                    onChange={this.onChange}
-                                    value={password}
-                                    tabIndex="3"
-                                    spellCheck="false"
-                                    autoComplete="new-password"
-                                    required
-                                />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label>Confirm Password</label>
-                            <div className="input-outer">
-                                <input
-                                    type="password"
-                                    className="form-control input"
-                                    name={randPassId2}
-                                    onChange={this.onChange}
-                                    value={password2}
-                                    tabIndex="4"
-                                    spellCheck="false"
-                                    autoComplete="new-password2"
-                                    required
-                                />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className="btn btn-slide" tabIndex="5">
-                                Register
-                                </button>
-                        </div>
-                        <p id={"login"}>
-                            Already have an account? <Link to="/login">Login</Link>
-                        </p>
-                    </form>
-                </div>
+    return (
+      <div className="col-md-6 m-auto upper-padding-register">
+        <div className="main">
+          <h2 className="text-center">Register</h2>
+          <br />
+          <form autoComplete="off" onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label>Username</label>
+              <div className="input-outer">
+                <input
+                  type="text"
+                  className="form-control input"
+                  name={randUserId}
+                  onChange={this.onChange}
+                  value={username}
+                  id={randUserId}
+                  tabIndex="1"
+                  spellCheck="false"
+                  autoComplete="new-username"
+                  required
+                />
+              </div>
             </div>
-        )
-    }
+            <div className="form-group">
+              <label>Email</label>
+              <div className="input-outer">
+                <input
+                  type="email"
+                  className="form-control input"
+                  name={randEmailId}
+                  onChange={this.onChange}
+                  value={email}
+                  tabIndex="2"
+                  spellCheck="false"
+                  autoComplete="new-email"
+                  required
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <div className="input-outer">
+                <input
+                  type="password"
+                  className="form-control input"
+                  name={randPassId}
+                  onChange={this.onChange}
+                  value={password}
+                  tabIndex="3"
+                  spellCheck="false"
+                  autoComplete="new-password"
+                  required
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label>Confirm Password</label>
+              <div className="input-outer">
+                <input
+                  type="password"
+                  className="form-control input"
+                  name={randPassId2}
+                  onChange={this.onChange}
+                  value={password2}
+                  tabIndex="4"
+                  spellCheck="false"
+                  autoComplete="new-password2"
+                  required
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-slide" tabIndex="5">
+                Register
+              </button>
+            </div>
+            <p id={"login"}>
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated,
-    user: state.auth.user,
-    isLoading: state.auth.isLoading
+  isAuthenticated: state.auth.isAuthenticated,
+  user: state.auth.user,
+  isLoading: state.auth.isLoading
 });
 
-export default withRouter(connect(mapStateToProps, { register, createMessage })(Register));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { register, createMessage }
+  )(Register)
+);
